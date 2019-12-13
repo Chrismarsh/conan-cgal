5cb0b3cc707448816db6f6d886e94b073e4f3487
Chris Marsh <chrismarsh.c2@gmail.com>
2019-12-13 15:06:10 -0600

    change arg location

--- build.py
+++ build.py
@@ -4,10 +4,10 @@ from collections import defaultdict
 if __name__ == "__main__":
     builder = ConanMultiPackager(
                                 archs=["x86_64"],
-                                build_types=["Release"],
-                                shared_option_name=False)
+                                build_types=["Release"])
                               
-    builder.add_common_builds(pure_c=False)
+    builder.add_common_builds(pure_c=False,
+                                shared_option_name=False)
 
     builder.remove_build_if(lambda build: build.settings["compiler.libcxx"] == "libstdc++")
 
